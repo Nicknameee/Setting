c8fa89e7b239717b48c65d88d8f36a2d91acd961
Nicknameee <igrunka1991@gmail.com>
2023-09-19 23:26:30 +0300

    Setting

---
+++ src/main/java/io/management/ua/Setting.java
@@ -0,0 +1,13 @@
+package io.management.ua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Setting {
+    public static void main(String[] args) {
+        SpringApplication.run(Setting.class);
+    }
+}
