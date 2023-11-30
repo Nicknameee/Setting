e1ddce11753367d8ee9357aeee2fd834331d2eee
Nicknameee <igrunka1991@gmail.com>
2023-11-30 22:00:00 +0200

    Props

--- src/main/java/io/management/ua/configuration/ApplicationSecurityConfiguration.java
+++ src/main/java/io/management/ua/configuration/ApplicationSecurityConfiguration.java
@@ -4,19 +4,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfiguration {
     @Bean
-    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
-
-        http.authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/encrypted/**", "/decrypted/**", "/actuator/**").permitAll()
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable);
+        http
+                .authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/encrypt", "/decrypt")
+                        .permitAll()
                 .anyRequest()
-                .authenticated())
-                .httpBasic(httpSecurityHttpBasicConfigurer -> {});
+                        .authenticated())
+                .httpBasic((httpSecurityHttpBasicConfigurer) -> {});
         return http.build();
     }
 }
