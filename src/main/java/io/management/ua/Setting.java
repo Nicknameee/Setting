37ba4d55d8df1b4fe8d74e46a00309aef567059e
Nicknameee <igrunka1991@gmail.com>
2024-02-15 00:34:54 +0200

    ELK stack added into logic

--- src/main/java/io/management/ua/Setting.java
+++ src/main/java/io/management/ua/Setting.java
@@ -1,13 +1,16 @@
 package io.management.ua;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@Slf4j
 @EnableConfigServer
 public class Setting {
     public static void main(String[] args) {
         SpringApplication.run(Setting.class);
+        log.debug("Configuration server started");
     }
 }
