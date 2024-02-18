3c94f978d599f41e0ebfbad9cccba06535f227c3
Nicknameee <igrunka1991@gmail.com>
2024-02-18 22:04:21 +0200

    Eureka

--- src/main/java/io/management/ua/Setting.java
+++ src/main/java/io/management/ua/Setting.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @Slf4j
 @EnableConfigServer
+@EnableEurekaServer
 public class Setting {
     public static void main(String[] args) {
         SpringApplication.run(Setting.class);
