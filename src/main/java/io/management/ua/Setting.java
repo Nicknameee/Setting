a7d9b3b9204fc698e85441f5028479eb219d3b21
Nicknameee <igrunka1991@gmail.com>
2024-02-22 13:13:06 +0200

    Discovery service separated from configuration

--- src/main/java/io/management/ua/Setting.java
+++ src/main/java/io/management/ua/Setting.java
@@ -3,13 +3,13 @@ package io.management.ua;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @Slf4j
 @EnableConfigServer
-@EnableEurekaServer
+@EnableDiscoveryClient
 public class Setting {
     public static void main(String[] args) {
         SpringApplication.run(Setting.class);
